65506396f5da031bae656b6202bdb5dafcfa786b
Nejc <nejcrogelsek0@gmail.com>
2021-07-31 19:56:07 +0200

    users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { User } from 'src/entities/user.entity';
+import { CreateUpdateUserDto } from './dto/create-update-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private usersService: UsersService) { }
+
     @Get()
     getUsers(): Promise<User[]> {
-        return
+        return this.usersService.findAll();
+    }
+
+    @Post('create')
+    createUser(@Body() body: CreateUpdateUserDto): Promise<User> {
+        return this.usersService.createUser(body);
+    }
+
+    @Patch('update')
+    updateUser(@Body() body: CreateUpdateUserDto, @Query('id', ParseIntPipe) id: number): Promise<User> {
+        return this.usersService.updateUser(id, body);
     }
 }
