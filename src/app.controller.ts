0a6a4273c2b1c45bfcb38b9c195813a4b7dc740c
Nejc <nejcrogelsek0@gmail.com>
2021-08-04 21:13:44 +0200

    login route + auth module imports/exports

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'This is NestJS API for project Quoastic'
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): Promise<{ access_token: string }> {
+    return this.authService.login(req.user);
   }
 }
