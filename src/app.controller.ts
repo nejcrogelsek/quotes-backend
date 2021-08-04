7b28a9f830a6fc63ac6cca55a7ae79018d4afdf6
Nejc <nejcrogelsek0@gmail.com>
2021-08-04 21:30:36 +0200

    adding jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { JwtAuthGuard } from './auth/auth-jwt.guard';
 import { AuthService } from './auth/auth.service';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { User } from './entities/user.entity';
 
 @Controller()
 export class AppController {
@@ -8,12 +10,18 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return 'This is NestJS API for project Quoastic'
+    return 'This is NestJS API for project Quoastic.'
   }
 
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Request() req): Promise<{ access_token: string }> {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('protected')
+  me(@Request() req): User {
+    return req.user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { User } from '../entities/user.entity';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService: UsersService) { }
+    constructor(private usersService: UsersService, private jwtService: JwtService) { }
 
     async validateUser(email: string, password: string): Promise<User> {
         const user = await this.usersService.findByEmail(email);
@@ -17,8 +18,9 @@ export class AuthService {
     }
 
     async login(user: User): Promise<{ access_token: string }> {
+        const payload = { name: user.first_name, sub: user.id }
         return {
-            access_token: 'SUCCESFULLY LOGGED IN!',
+            access_token: this.jwtService.sign(payload),
         }
     }
 }
