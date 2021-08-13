4c3da65da1e675f51f0ab6429ae4175f8e9c91ea
Nejc <nejcrogelsek0@gmail.com>
2021-08-13 18:28:14 +0200

    move login route to users controller

--- src/modules/app.controller.ts
+++ src/modules/app.controller.ts
@@ -7,28 +7,9 @@ import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService, private usersService: UsersService) { }
-
   @Get()
   getHello(): string {
     return 'This is NestJS API for project Quotastic.'
-  }
-
-  @UseGuards(LocalAuthGuard)
-  @Post('login')
-  async login(@Request() req): Promise<AuthReturnData> {
-    const { access_token } = await this.authService.login(req.user);
-    const { id, email, first_name, last_name, profile_image } = await this.usersService.findByEmail(req.user.email)
-    return {
-      user: {
-        id,
-        email,
-        first_name,
-        last_name,
-        profile_image
-      },
-      access_token
-    };
   }
 
   @UseGuards(JwtAuthGuard)
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Patch, Post, Res, } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Request, Param, ParseIntPipe, Patch, Post, Res, UseGuards, forwardRef, Inject, } from '@nestjs/common';
 import { Response } from 'express';
 import { AuthReturnData } from 'src/interfaces/auth.interface';
 import { generateUploadUrl } from '../../../s3'
 import { User } from '../../entities/user.entity';
+import { AuthService } from '../auth/auth.service';
+import { LocalAuthGuard } from '../auth/local-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private usersService: UsersService) { }
+    constructor(private usersService: UsersService,
+        @Inject(forwardRef(() => AuthService)) private readonly authService: AuthService) { }
 
     @Get('/find-by-email')
     test(@Body() body: { email: string }): Promise<User> {
@@ -41,5 +44,22 @@ export class UsersController {
     async uploadFile(@Res() res: Response) {
         const url = await generateUploadUrl();
         res.send({ url });
+    }
+
+    @UseGuards(LocalAuthGuard)
+    @Post('login')
+    async login(@Request() req): Promise<AuthReturnData> {
+        const { access_token } = await this.authService.login(req.user);
+        const { id, email, first_name, last_name, profile_image } = await this.usersService.findByEmail(req.user.email)
+        return {
+            user: {
+                id,
+                email,
+                first_name,
+                last_name,
+                profile_image
+            },
+            access_token
+        };
     }
 }
