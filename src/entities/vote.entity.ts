b9b8df53217736e49e878b290c4eaedf699975e8
Nejc <nejcrogelsek0@gmail.com>
2021-08-19 16:55:51 +0200

    vote entity

---
+++ src/entities/vote.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { Quote } from './quote.entity';
+import { User } from './user.entity';
+
+@Entity()
+export class Vote {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    quote_id: number;
+
+    @Column()
+    user_id: number;
+
+    @ManyToOne(() => Quote, vote => vote.votes)
+    vote: Quote;
+}
