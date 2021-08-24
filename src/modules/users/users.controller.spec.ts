14832026a536ace75793de30ad0f6357e0660d3e
Nejc <nejcrogelsek0@gmail.com>
2021-08-24 18:38:42 +0200

    users.controller.spec - create/update fn

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -6,7 +6,22 @@ describe('UsersController', () => {
   let controller: UsersController;
 
   const mockUserService = {
-
+    createUser: jest.fn(dto => {
+      return {
+        id: expect.any(Number),
+        ...dto,
+      }
+    }),
+    updateUser: jest.fn((dto) => ({
+      id: expect.any(Number),
+      email: expect.any(String),
+      first_name: expect.any(String),
+      last_name: expect.any(String),
+      profile_image: expect.any(String),
+      password: expect.any(String),
+      created_at: expect.any(String),
+      updated_at: expect.any(String)
+    }))
   }
 
   beforeEach(async () => {
@@ -21,4 +36,51 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a user', () => {
+    const dto = {
+      profile_image: 'undefined',
+      email: 'mockUser@gmail.com',
+      first_name: 'Mock',
+      last_name: 'User',
+      password: 'Mock123!',
+      confirm_password: 'Mock123!'
+    };
+    expect(controller.createUser(dto)).toEqual({
+      user: {
+        id: expect.any(Number),
+        email: 'mockUser@gmail.com',
+        first_name: 'Mock',
+        last_name: 'User',
+        profile_image: 'undefined'
+      },
+      access_token: expect.any(String)
+    })
+
+    expect(mockUserService.createUser).toHaveBeenCalledWith(dto);
+  })
+
+  it('should update a user', () => {
+    const dto = {
+      id: 14,
+      email: 'neki@gmail.com',
+      first_name: 'Mock Updated',
+      last_name: 'Uporabnik',
+      password: 'Neki123!',
+      confirm_password: 'Neki123!'
+    };
+    expect(controller.updateUser(dto)).toEqual({
+      id: 14,
+      email: 'neki@gmail.com',
+      first_name: 'Mock Updated',
+      last_name: 'Uporabnik',
+      profile_image: expect.any(String),
+      password: expect.any(String),
+      created_at: expect.any(String),
+      updated_at: expect.any(String)
+    })
+
+    expect(mockUserService.updateUser).toHaveBeenCalledWith(dto);
+  })
+
 });
